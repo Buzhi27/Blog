be35f4538d66f1294a3312727a3ad983a34f94c6
buzhi <wyh0481@outlook.com>
2024-05-03 17:28:23 +0800

    update host

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,11 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: {
-      // 给前端服务器设置了代理，即将 /api 地址的前端请求转发到 8000 端口的后端服务器去，从而规避跨域问题。
-      '/api': {
-        target: `http://127.0.0.1:8000/api`,
+      "/api": {
+        target: `http://47.97.196.12/api`,
         changeOrigin: true,
         pathRewrite: {
-          '^/api': '',
+          "^/api": "",
         },
       },
     },
